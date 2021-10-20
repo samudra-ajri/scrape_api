e8ee2ed3838d2776f36e87f07e7a56b99aea5c5a
samudra-ajri <samudra_ajri@live.com>
2021-10-20 17:37:03 +0700

    Add mongodb pipeline

--- scrape_api/pipelines.py
+++ scrape_api/pipelines.py
@@ -4,8 +4,18 @@
 #
 # Don't forget to add your pipeline to the ITEM_PIPELINES setting
 # See: https://doc.scrapy.org/en/latest/topics/item-pipeline.html
+import pymongo
 
+class MongodbPipeline(object):
+    collection_name = 'contents'
 
-class ScrapeApiPipeline(object):
+    def open_spider(self, spider):
+        self.client = pymongo.MongoClient('')
+        self.db = self.client['Scrapy']
+
+    def close_spider(self, spider):
+        self.client.close()
+
     def process_item(self, item, spider):
+        self.db[self.collection_name].insert(item)
         return item
